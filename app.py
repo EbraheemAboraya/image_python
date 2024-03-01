4ecb5410c5fdd004e43ac99a677a41426a88d8ad
EbraheemAboraya <ibrahem.aboraya2002@gmail.com>
2024-03-01 16:22:54 +0200

    new change port

--- app.py
+++ app.py
@@ -47,4 +47,5 @@ def predict():
     return jsonify({'category': category})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))  # Use the PORT environment variable if it's set, otherwise default to 5000
+    app.run(debug=False, host='0.0.0.0', port=port)
